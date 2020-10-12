7e02688dac9f600771cde0ac7fac316497e8dd4f
mikohan <angara99@gmail.com>
2020-10-12 18:11:01 +0300

    Second lesson

--- src/components/TestMatereal.jsx
+++ src/components/TestMatereal.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'red',
+    color: 'blue',
+  },
+  headStyle: {
+    color: 'darkblue',
+  },
+});
 
 function TestMatereal(props) {
+  const classes = useStyles();
+
   return (
     <div>
-      <h1>Im an test material application</h1>
+      <Typography className={classes.headStyle} color="primary" variant="h2">
+        Material UI Application
+      </Typography>
       <Button color="primary" variant="outlined">
         Im a button
       </Button>
